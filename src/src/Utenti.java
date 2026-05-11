6c08e351c1a8f2e2199a35c61d3c97b0bbe3c434
kauan <kausalmazo@gmail.com>
2026-05-11 09:51:00 -0300

    testing utenti e fileReader

--- src/src/Utenti.java
+++ src/src/Utenti.java
@@ -7,10 +7,11 @@ public class Utenti {
     public static void main(String[] args) throws FileNotFoundException {
         ConsoleOutputManager out = new ConsoleOutputManager();
 
-    File file = new File("D:\\IntelliJ\\Projetos\\CineMaxGrupo7CO-LabA\\src\\src");
+    File file = new File("D:\\IntelliJ\\Projetos\\CineMaxGrupo7CO-LabA\\src\\src\\UntentiIInfo.txt");
     Scanner scan = new Scanner(file);
 
-    out.println(scan.nextLine());
+    while(scan.hasNextLine()){
+    out.println(scan.nextLine());}
 
 
     }
