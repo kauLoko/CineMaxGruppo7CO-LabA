c499f6fe18adb3fff995ff30c2f41e121d700c0f
kauan <kausalmazo@gmail.com>
2026-05-11 09:05:38 -0300

    Cinemax

--- src/src/CineMax.java
+++ src/src/CineMax.java
@@ -5,7 +5,57 @@ public class CineMax {
         ConsoleOutputManager out = new ConsoleOutputManager();
         ConsoleInputManager in = new ConsoleInputManager();
 
-        String oi = in.readLine("escolha:");
-        out.println(oi);
+        int scelta = in.readInt("Buon giorno! Sceigli como voule logare: \n 1.Clienti\n2.Proiezionista\n3.bigliettai\n4.Non registrati\n");
+        // Scelta diversa
+        if ((scelta != 1) & (scelta != 2) & (scelta != 3) & (scelta != 4)){
+            out.println("scelta non valida");
+        }
+
+        //clienti
+        if (scelta == 1) {
+            scelta = in.readInt("Scegli:\n 1.login\n 2.Registrare\n");
+            if ((scelta != 1) & (scelta != 2)){
+                out.println("scelta non valida");
+            }
+            if (scelta == 1) {
+                //fare login
+            }
+            if (scelta == 2) {
+                //Registrare
+            }
+        }
+
+        //Proiezionista
+        if (scelta == 2) {
+            scelta = in.readInt("Scegli:\n 1.login\n 2.Registrare\n");
+            if ((scelta != 1) & (scelta != 2) ){
+                out.println("scelta non valida");
+            }
+            if (scelta == 1) {
+                //fare login
+            }
+            if (scelta == 2) {
+                //Registrare
+            }
+        }
+
+        //Bigletai
+        if (scelta == 3) {
+            scelta = in.readInt("Scegli:\n 1.login\n 2.Registrare\n");
+            if ((scelta != 1) & (scelta != 2) ){
+                out.println("scelta non valida");
+            }
+            if (scelta == 1) {
+                //fare login
+            }
+            if (scelta == 2) {
+                //Registrare
+            }
+        }
+
+        //non registrato
+        if (scelta == 4) {
+            // trovare Film
+        }
     }
 }
