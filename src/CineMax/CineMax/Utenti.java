2120a98b3849dce6dfbf1b4392cb3fca510f3a16
kauan <kausalmazo@gmail.com>
2026-05-12 14:20:50 -0300

    class Registrare

--- src/CineMax/CineMax/Utenti.java
+++ src/CineMax/CineMax/Utenti.java
@@ -1,5 +1,11 @@
 package CineMax;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Utenti {
     //Campi
     private String nome;
@@ -8,9 +14,11 @@ public class Utenti {
     private int password;
     private String nascista;
     private String domicilio;
-    private char role;
+    private String role;
+
     //Construtore
-    public Utenti(String na,String cg,String user,int ps, String nsct,String dom,char ro){
+
+    public Utenti(String na,String cg,String user,int ps, String nsct,String dom,String ro){
         nome = na;
         cognome = cg;
         username = user;
@@ -19,7 +27,30 @@ public class Utenti {
         domicilio = dom;
         role = ro;
     }
+
     //Metodi
+
+    public void registraCliente() throws IOException {
+        File file = new File("UntentiIInfo2.txt");
+        Scanner scan = new Scanner(file);
+        String fileContent = "" ;
+
+        while (scan.hasNextLine()) {
+            fileContent = fileContent.concat(scan.nextLine() + "\n");
+        }
+        BufferedWriter writer = new BufferedWriter(new FileWriter("UntentiIInfo2.txt"));
+        writer.write(fileContent + nome + " " + cognome + " "  + username + " " + password + " " + nascista + " " +domicilio + " " + role);
+        writer.close();
+    }
+
+    public void usernameLogin(){
+        String userLog = username;
+    }
+
+    public void passwordLogin(){
+        int passLog = password;
+    }
+
     public String toString(){
         return "\nIl tuo  è Utenti:" + nome + " " + cognome +"\nUsername: "+ username + "\nPassword: " + String.valueOf(password)
                 + "\nNascita: " + nascista + "\nDomicilio: " + domicilio + "\nRole: " + role;
