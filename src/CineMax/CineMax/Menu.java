54d6a2e31ede291ed649af270ad86e0613dad358
kauan <kausalmazo@gmail.com>
2026-05-16 09:29:51 -0300

    All menu Models Update

--- src/CineMax/CineMax/Menu.java
+++ src/CineMax/CineMax/Menu.java
@@ -52,13 +52,13 @@ public class Menu
 
             //Acesso autorizato
             if (autenticazione ) {
-                out.println("Login bene-sucedido! Bene-venuto " + username);
+                out.println("Login bene-sucedido!\n\nBene-venuto " + username + "!!");
 
-                //tipo di Menu a Aprire
+                //Tipo di Menu a Aprire
                 try (fileScanner) {
                     {
-
                         if (dados[2].equals(username) && dados[3].equals(password)) {
+
                             //Tipo Menu:Clienti
                             if (dados[6].equals("Clienti")){
                                 int sceltaClienti = 0;
@@ -67,27 +67,91 @@ public class Menu
                                     sceltaClienti = in.readInt("1.Cercare proiezioni\n2.Visualizzare le proprie prenotazioni\n3.Modificare e cancellare le proprie prenotazioni\n4.Logout\n");
                                     if (sceltaClienti == 1 || sceltaClienti == 2 || sceltaClienti == 3 || sceltaClienti == 4){
                                         continua = false;
+
+                                        //Cercare proiezioni
+                                        if(sceltaClienti == 1){
+
+                                        }
+                                        //Visualizzare le proprie prenotazioni
+                                        else if(sceltaClienti == 2){
+
+                                        }
+                                        //Modificare e cancellare le proprie prenotazioni
+                                        else if(sceltaClienti == 3){
+
+                                        }
+                                        //Logout
+                                        else if(sceltaClienti == 4){
+
+                                        }
                                     }
                                     else{
                                         out.println("\nScelta non valida!");
                                     }
                                 }
-
-
-
-
                             }
 
                             //Tipo Menu:Proiezionista
                             else if (dados[6].equals("Proiezionista")){
-                                out.println("Proiezionista");
+                                int sceltaProiezionista = 0;
+                                boolean continua = true;
+                                while(continua) {
+                                    sceltaProiezionista = in.readInt("1.Inserire un film\n2.Modificare la data di una proiezione\n3.Eliminare una proiezione\n4.Logout\n");
+                                    if (sceltaProiezionista == 1 || sceltaProiezionista == 2 || sceltaProiezionista == 3 || sceltaProiezionista == 4){
+                                        continua = false;
+
+                                        //Inserire un film
+                                        if(sceltaProiezionista == 1){
+                                            aggiungiProiezione(scanner);
+                                        }
+                                        //Modificare la data di una proiezione
+                                        else if(sceltaProiezionista == 2){
+
+                                        }
+                                        //Eliminare una proiezione
+                                        else if(sceltaProiezionista == 3){
+
+                                        }
+                                        //Logout
+                                        else if(sceltaProiezionista == 4){
+
+                                        }
+
+                                    //Scelta non valida
+                                    }else{
+                                        out.println("\nScelta non valida!");
+                                    }
+                                }
 
                             }
 
                             //Tipo Menu:Balconista
                             else if (dados[6].equals("Balconista")){
-                                out.println("Balconista");
+                                int sceltaBalconista = 0;
+                                boolean continua = true;
+                                while(continua) {
+                                    sceltaBalconista = in.readInt("1.Visualizzare le prenotazioni nella data odierna\n2.Cercare una prenotazione\n3.Logout\n");
+                                    if (sceltaBalconista == 1 || sceltaBalconista == 2 || sceltaBalconista == 3){
+                                        continua = false;
 
+                                        //IVisualizzare le prenotazioni nella data odierna
+                                        if(sceltaBalconista == 1){
+
+                                        }
+                                        //Cercare una prenotazione
+                                        else if(sceltaBalconista == 2){
+
+                                        }
+                                        //Logout
+                                        else if(sceltaBalconista == 3){
+
+                                        }
+
+                                    //Scelta non valida
+                                    }else{
+                                        out.println("\nScelta non valida!");
+                                    }
+                                }
                             }
                         }
                     }
