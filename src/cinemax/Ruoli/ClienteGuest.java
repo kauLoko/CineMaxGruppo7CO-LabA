064cf185cb52861631c7a63df99f4853b755d481
Mattia <mattialanza22@gmail.com>
2026-08-23 16:46:41 +0200

    creazione menu utente guest

--- src/cinemax/Ruoli/ClienteGuest.java
+++ src/cinemax/Ruoli/ClienteGuest.java
@@ -1,4 +1,28 @@
+import java.util.*;
+import java.io.*;
+
 public class ClienteGuest 
 {
+    public static menuGuest() 
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("\nInserisci il titolo del film e premi invio per avviare la ricerca:");
         
+        String titolo = scanner.nextLine().trim();
+
+        List<Proiezione> proiezioni = .cercaProiezioni(titolo, null, null, null, null, null);
+
+    
+
+        while (true) {
+            System.out.println("\n--- OPZIONI GUEST ---");
+            System.out.println("1. Cerca Proiezioni (Filtri Avanzati)");
+            System.out.println("2. Visualizza Dettaglio Proiezione");
+            System.out.println("0. Torna al Menu Principale");
+            System.out.print("Scelta: ");
+
+            String scelta = scanner.nextLine().trim();
+        }
+    }
 }
